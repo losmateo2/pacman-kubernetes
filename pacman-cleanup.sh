36047f8f2c18457b0f13175997adf99d32c88f6e
losmateo2 <19825488+losmateo2@users.noreply.github.com>
2024-04-01 13:59:00 -0500

    update for openshift

--- pacman-cleanup.sh
+++ pacman-cleanup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-kubectl delete -n pacman -f security/rbac.yaml
-kubectl delete -n pacman -f security/secret.yaml
-kubectl delete -n pacman -f deployments/mongo-deployment.yaml
-kubectl delete -n pacman -f deployments/pacman-deployment.yaml
-kubectl delete -n pacman -f services/mongo-service.yaml
-kubectl delete -n pacman -f services/pacman-service.yaml
-kubectl delete -n pacman -f persistentvolumeclaim/mongo-pvc.yaml
-kubectl delete namespace pacman
+oc delete -n pacman -f security/rbac.yaml
+oc delete -n pacman -f security/secret.yaml
+oc delete -n pacman -f deployments/mongo-deployment.yaml
+oc delete -n pacman -f deployments/pacman-deployment.yaml
+oc delete -n pacman -f services/mongo-service.yaml
+oc delete -n pacman -f services/pacman-service.yaml
+oc delete -n pacman -f persistentvolumeclaim/mongo-pvc.yaml
+oc delete namespace pacman
--- pacman-install.sh
+++ pacman-install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-kubectl create namespace pacman
-kubectl create -n pacman -f security/rbac.yaml
-kubectl create -n pacman -f security/secret.yaml
-kubectl create -n pacman -f persistentvolumeclaim/mongo-pvc.yaml
-kubectl create -n pacman -f deployments/mongo-deployment.yaml
-while [ "$(kubectl get pods -l=name='mongo' -n pacman -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
+oc create namespace pacman
+oc create -n pacman -f security/rbac.yaml
+oc create -n pacman -f security/secret.yaml
+oc create -n pacman -f persistentvolumeclaim/mongo-pvc.yaml
+oc create -n pacman -f deployments/mongo-deployment.yaml
+while [ "$(oc get pods -l=name='mongo' -n pacman -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
    sleep 5
    echo "Waiting for mongo pod to change to running status"
 done
-kubectl create -n pacman -f deployments/pacman-deployment.yaml
-kubectl create -n pacman -f services/mongo-service.yaml
-kubectl create -n pacman -f services/pacman-service.yaml
+oc create -n pacman -f deployments/pacman-deployment.yaml
+oc create -n pacman -f services/mongo-service.yaml
+oc create -n pacman -f services/pacman-service.yaml
